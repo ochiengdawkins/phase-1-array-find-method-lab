f2d4730f8dd1668f6b6fbd49f12cff4a31225dc3
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 12:52:08 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arrys){
+    let year;
+    year = arrys.find(({result}) => result === "W");
+    if(typeof(year) == "object"){
+        return year["year"];
+    }
+    return year;
+}
